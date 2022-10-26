3cbb69e00dfc049be8df5f5ec7843950b323ac76
Juliana Daroz <julianadaroz@gmail.com>
2022-10-26 13:28:57 -0300

    Finished the useContext part and created hooks folder.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 //imports
-import { useState, FormEvent } from "react";
+import { useState, FormEvent, useContext } from "react";
 import Modal from "react-modal";
-import { api } from "../../services/api";
+import { useTransactions } from "../../hooks/useTransactions";
 
 //icons
 import incomeImg from "../../assets/income.svg";
@@ -20,22 +20,28 @@ export function NewTransactionModal({
     isOpen,
     onRequestClose,
 }: NewTransactionModalProps) {
+    const { createTransaction } = useTransactions();
+
     const [title, setTitle] = useState("");
-    const [value, setValue] = useState(0);
+    const [amount, setAmount] = useState(0);
     const [type, setType] = useState("deposit");
     const [category, setCategory] = useState("");
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        const data = {
+        await createTransaction({
             title,
-            value,
+            amount,
             type,
             category,
-        };
+        });
 
-        api.post("/transactions", data);
+        setTitle("");
+        setAmount(0);
+        setType("deposit");
+        setCategory("");
+        onRequestClose();
     }
 
     return (
@@ -62,8 +68,8 @@ export function NewTransactionModal({
                 <input
                     placeholder="Valor"
                     type="number"
-                    value={value}
-                    onChange={(event) => setValue(Number(event.target.value))}
+                    value={amount}
+                    onChange={(event) => setAmount(Number(event.target.value))}
                 />
                 <TransactionTypeContainer>
                     <RadioBox
